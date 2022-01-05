29534f38267509a3d3d8bc034c5e2b4b8f6b3a32
Arsh Meharwal <arshmeharwal98@gmail.com>
2022-01-05 12:18:00 +0100

    Update app.js

--- app.js
+++ app.js
@@ -10,25 +10,25 @@ function clicked() {
     let clickYear = date.getFullYear();
     let clickMonth = date.getMonth();
     let cTime = date.getHours();
+    console.log(clickDay);
+    console.log(cTime);
 
     var val = document.getElementById("input");
     var inputValue = val.value;
-    console.log(inputValue);
-    console.log(cTime);
 
     //Day Change
     if (clickDay === currentDay) {
-        let prevVal = localStorage.getItem('today')
+        let prevVal = localStorage.getItem(currentDay)
         let newVal = Number(prevVal) + Number(inputValue)
-        localStorage.setItem('today', Number(newVal));
+        localStorage.setItem(currentDay, Number(newVal));
     } else {
-        localStorage.setItem('today', Number(inputValue));
+        localStorage.setItem(currentDay, Number(inputValue));
     }
 
 
     if (clickMonth === currentMonth) {
         let prevVal = localStorage.getItem('month')
-        let newVal = Number(prevVal) + Number(inputValue);
+        let newVal = Number(prevVal) + Number(inputValue)
         localStorage.setItem('month', Number(newVal));
     } else {
         localStorage.setItem('month', Number(inputValue));
@@ -43,9 +43,49 @@ function clicked() {
         localStorage.setItem('year', Number(inputValue));
     }
     let showElm = document.getElementById("set");
-    showElm.innerHTML = `<h1>Today Rs. ${localStorage.getItem('today')}<h1> <h1>This Month Rs. ${localStorage.getItem('month')}<h1><h1>This Year Rs. ${localStorage.getItem('year')}<h1>`
+    showElm.innerHTML = `<h1>Today Rs. ${localStorage.getItem(currentDay)}<h1> <h1>This Month Rs. ${localStorage.getItem('month')}<h1><h1>This Year Rs. ${localStorage.getItem('year')}<h1>`
     val.value = "";
+    JSC.Chart('chartDiv', {
+        type: 'vertical column aqua',
+        series: [
+            {
+                points: [
+                    { x: '0', y: Number(localStorage.getItem('0')) },
+                    { x: '1', y: Number(localStorage.getItem('1')) },
+                    { x: '2', y: Number(localStorage.getItem('2')) },
+                    { x: '3', y: Number(localStorage.getItem('3')) },
+                    { x: '4', y: Number(localStorage.getItem('4')) },
+                    { x: '5', y: Number(localStorage.getItem('5')) },
+                    { x: '6', y: Number(localStorage.getItem('6')) },
+                    { x: '7', y: Number(localStorage.getItem('7')) },
+                    { x: '8', y: Number(localStorage.getItem('8')) },
+                    { x: '9', y: Number(localStorage.getItem('9')) },
+                    { x: '10', y: Number(localStorage.getItem('10')) },
+                    { x: '11', y: Number(localStorage.getItem('11')) },
+                    { x: '12', y: Number(localStorage.getItem('12')) },
+                    { x: '13', y: Number(localStorage.getItem('13')) },
+                    { x: '14', y: Number(localStorage.getItem('14')) },
+                    { x: '15', y: Number(localStorage.getItem('15')) },
+                    { x: '16', y: Number(localStorage.getItem('16')) },
+                    { x: '17', y: Number(localStorage.getItem('17')) },
+                    { x: '18', y: Number(localStorage.getItem('18')) },
+                    { x: '19', y: Number(localStorage.getItem('19')) },
+                    { x: '20', y: Number(localStorage.getItem('20')) },
+                    { x: '21', y: Number(localStorage.getItem('21')) },
+                    { x: '22', y: Number(localStorage.getItem('22')) },
+                    { x: '23', y: Number(localStorage.getItem('23')) },
+                    { x: '24', y: Number(localStorage.getItem('24')) },
+                    { x: '25', y: Number(localStorage.getItem('25')) },
+                    { x: '26', y: Number(localStorage.getItem('26')) },
+                    { x: '27', y: Number(localStorage.getItem('27')) },
+                    { x: '28', y: Number(localStorage.getItem('28')) },
+                    { x: '29', y: Number(localStorage.getItem('29')) },
+                    { x: '30', y: Number(localStorage.getItem('30')) },
 
+                ]
+            }
+        ]
+    });
 
 
 }
@@ -56,4 +96,46 @@ function press(event) {
 }
 
 let showElm = document.getElementById("set");
-showElm.innerHTML = `<h1>Today Rs. ${localStorage.getItem('today')}<h1> <h1>This Month Rs. ${localStorage.getItem('month')}<h1><h1>This Year Rs. ${localStorage.getItem('year')}<h1>`
+showElm.innerHTML = `<h1>Today Rs. ${localStorage.getItem(currentDay)}<h1> <h1>This Month Rs. ${localStorage.getItem('month')}<h1><h1>This Year Rs. ${localStorage.getItem('year')}<h1>`
+
+
+JSC.Chart('chartDiv', {
+    type: 'vertical column aqua',
+    series: [
+        {
+            points: [
+                { x: '0', y: Number(localStorage.getItem('0')) },
+                { x: '1', y: Number(localStorage.getItem('1')) },
+                { x: '2', y: Number(localStorage.getItem('2')) },
+                { x: '3', y: Number(localStorage.getItem('3')) },
+                { x: '4', y: Number(localStorage.getItem('4')) },
+                { x: '5', y: Number(localStorage.getItem('5')) },
+                { x: '6', y: Number(localStorage.getItem('6')) },
+                { x: '7', y: Number(localStorage.getItem('7')) },
+                { x: '8', y: Number(localStorage.getItem('8')) },
+                { x: '9', y: Number(localStorage.getItem('9')) },
+                { x: '10', y: Number(localStorage.getItem('10')) },
+                { x: '11', y: Number(localStorage.getItem('11')) },
+                { x: '12', y: Number(localStorage.getItem('12')) },
+                { x: '13', y: Number(localStorage.getItem('13')) },
+                { x: '14', y: Number(localStorage.getItem('14')) },
+                { x: '15', y: Number(localStorage.getItem('15')) },
+                { x: '16', y: Number(localStorage.getItem('16')) },
+                { x: '17', y: Number(localStorage.getItem('17')) },
+                { x: '18', y: Number(localStorage.getItem('18')) },
+                { x: '19', y: Number(localStorage.getItem('19')) },
+                { x: '20', y: Number(localStorage.getItem('20')) },
+                { x: '21', y: Number(localStorage.getItem('21')) },
+                { x: '22', y: Number(localStorage.getItem('22')) },
+                { x: '23', y: Number(localStorage.getItem('23')) },
+                { x: '24', y: Number(localStorage.getItem('24')) },
+                { x: '25', y: Number(localStorage.getItem('25')) },
+                { x: '26', y: Number(localStorage.getItem('26')) },
+                { x: '27', y: Number(localStorage.getItem('27')) },
+                { x: '28', y: Number(localStorage.getItem('28')) },
+                { x: '29', y: Number(localStorage.getItem('29')) },
+                { x: '30', y: Number(localStorage.getItem('30')) },
+            ]
+        }
+    ]
+});
